afbfc9030583fbde08eafe3562db268a53e78025
root <root@ubuntu.(none)>
2013-05-17 22:22:42 +0800

    finish parse the time and config file

--- server.c
+++ server.c
@@ -22,6 +22,7 @@
 #include	<stdio.h>
 #include	<sys/types.h>
 #include	<sys/socket.h>
+#include	<netinet/in.h> //for sockaddr_in
 #include	<sys/stat.h>
 #include	<time.h>
 #include	<string.h>
@@ -38,6 +39,54 @@
 	int
 main ( int argc, char *argv[] )
 {
+	int sock;
+	FILE* conf;
+	char buf_conf[4096];
+	char* port_str;
+	char* path;
+	char* pos;
+	int PORT;
+
+/* ----------------------- start parse the http.conf ------------------------------- */
+conf = fopen("http.conf", "rb");
+if(conf == NULL)
+{
+	printf("Can't open the http.conf file\r\n");
+	exit(1);
+}
+
+if(!fgets(buf_conf, sizeof(buf_conf), conf))
+	return -1;
+	
+port_str	 =	strtok(buf_conf, " ");
+path		 =	strtok(NULL, " ");
+pos			 =	strchr(path, '\n');
+
+if(pos)
+	path[pos-path] = 0;
+
+PORT = atoi(port_str);
+fclose(conf);
+
+/* ------------------------ end -------------------------------------------------- */
+
+/* ----------------------use socket to implement the http protocol---------------- */
+
+struct sockaddr_in server_addr;
+
+sock = socket(AF_INET, SOCK_STREAM, 0);
+
+server_addr.sin_family = AF_INET;
+server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+server_addr.sin_port = htons(PORT);
+bind(sock, (struct sockaddr *) &server_addr, sizeof(server_addr));
+
+listen(sock, 50);
+printf("HTTP server listening on port %d at %s \n", PORT, inet_ntoa(server_addr.sin_addr));
+
+
+/* ---------------------------end------------------------------------------------- */
+
 	
 	return EXIT_SUCCESS;
 }				/* ----------  end of function main  ---------- */
